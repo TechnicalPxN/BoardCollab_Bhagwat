44c7893b4eefca9945a14902358c7fc9b217c9dc
Bhagwat Nagare <141098047+TechnicalPxN@users.noreply.github.com>
2026-03-12 13:27:21 +0530

    Initialize App component with CanvasBoard

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CanvasBoard from "./components/CanvasBoard";
+
+function App(){
+ return (
+   <div>
+     <CanvasBoard roomId="demo-room"/>
+   </div>
+ );
+}
+
+export default App;
